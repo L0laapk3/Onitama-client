b607298518df0b6f311a19f096766630d6f9fc9f
L0laapk3 <L0laapk3@gmail.com>
2020-11-10 21:00:12 +0100

    added cards

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
 'use strict';
 
+const cards = {};
+for (let card of CARDS.base)
+	cards[card.name] = card;
+
+class Card {
+	constructor(flipped) {
+		this.el = document.createElement("game-card");
+		if (flipped)
+			this.el.setAttribute("flipped", "");
+		this.flipped = flipped;
+		this.gridEl = document.createElement("game-card-grid");
+		for (let i = 0; i < 25; i++)
+			this.gridEl.append(document.createElement("game-card-grid-cell"));
+		this.el.append(this.gridEl);
+		this.nameEl = document.createElement("game-card-name");
+		this.el.append(this.nameEl);
+	}
+	set(name) {
+		const type = cards[name];
+		this.nameEl.innerText = name;
+		for (let i = 0; i < 25; i++) {
+			this.gridEl.children[i].setAttribute("possible", type.moves[this.flipped ? 24 - i : i]);
+		}
+	}
+}
 const board = {
-	el: document.getElementsByTagName("game-board")[0],
+	cards: {
+		top: [new Card(true), new Card(true)],
+		side: new Card(),
+		bottom: [new Card(), new Card()],
+	},
+	el: document.createElement("game-board"),
 	cells: [[], [], [], [], []]
 };
+const container = document.getElementsByTagName("game-container")[0];
+const boardContainer = document.createElement("board-container");
+boardContainer.append(board.cards.top[0].el);
+boardContainer.append(board.cards.top[1].el);
+boardContainer.append(board.el);
+boardContainer.append(board.cards.bottom[0].el);
+boardContainer.append(board.cards.bottom[1].el);
+container.append(boardContainer);
+container.append(board.cards.side.el);
 for (let y = 0; y < 5; y++) {
 	const rowEl = document.createElement("game-board-row");
 	for (let x = 0; x < 5; x++) {
@@ -18,7 +57,27 @@ for (let y = 0; y < 5; y++) {
 	}
 	board.el.append(rowEl);
 }
+
+
 function setBoard(data) {
+	const participating = localStorage["match-" + data.matchId];
+	const flipped = participating && participating[0] == "B";
+	const token = participating && participating.substr(1);
+	if (flipped) {
+		board.el.setAttribute("flipped", "");
+		board.cards.red = board.cards.top;
+		board.cards.blue = board.cards.bottom;
+	} else {
+		board.cards.red = board.cards.bottom;
+		board.cards.blue = board.cards.top;
+		board.el.removeAttribute("flipped");
+	}
+	board.cards.blue[0].set(data.cards.blue[0]);
+	board.cards.blue[1].set(data.cards.blue[1]);
+	board.cards.side.set(data.cards.side);
+	board.cards.red[0].set(data.cards.red[0]);
+	board.cards.red[1].set(data.cards.red[1]);
+
 	let newPieces = [[], [], [], []], i = 0, removedPieces = [[], [], [], []];
 	for (let y = 0; y < 5; y++)
 		for (let x = 0; x < 5; x++) {
@@ -31,7 +90,6 @@ function setBoard(data) {
 			}
 			i++;
 		}
-		console.log(newPieces, removedPieces)
 	for (let type = 0; type < 4; type++) {
 		let i = 0;
 		for (; i < Math.min(newPieces[type].length, removedPieces[type].length); i++) {
@@ -74,15 +132,18 @@ const ws = new WebSocket("wss://litama.herokuapp.com");
 ws.onerror = initialiseMainPage;
 ws.onmessage = e => {
 	const data = JSON.parse(e.data);
+	console.log(data);
 	switch(data.messageType) {
 	case "state":
+		let joining = false;
 		switch(data.gameState) {
 		case "waiting for player":
 			ws.send("join " + data.matchId);
+			joining = true;
 		case "in progress":
 			if (!subscribed)
 				ws.send("spectate " + data.matchId);
-			if (data.gameState == "waiting for player")
+			if (joining)
 				break;
 			setBoard(data);
 			break;
@@ -91,11 +152,17 @@ ws.onmessage = e => {
 	case "spectate":
 		subscribed = true;
 		break;
+	case "join":
+	case "create":
+		localStorage["match-" + data.matchId] = (data.color == "red" ? "R" : "B") + data.token;
+		break;
 	case "error":
 	default:
 		console.error(data);
-		if (data.command == "state")
+		if (data.command == "state") {
+			delete localStorage["match-" + match[1]];
 			initialiseMainPage();
+		}
 		throw Error();
 	}
 };
