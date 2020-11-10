016c8f203d87d3badf3bb38af035fe97a1162d47
L0laapk3 <L0laapk3@gmail.com>
2020-11-10 23:44:10 +0100

    playable

--- index.js
+++ index.js
@@ -19,9 +19,11 @@ class Card {
 	}
 	set(name) {
 		const type = cards[name];
+		this.name = name;
 		this.nameEl.innerText = name;
+		this.moves = this.flipped ? type.moves : type.moves.split("").reverse().join("");
 		for (let i = 0; i < 25; i++) {
-			this.gridEl.children[i].setAttribute("possible", type.moves[this.flipped ? i : 24 - i]);
+			this.gridEl.children[i].setAttribute("possible", this.moves[i]);
 		}
 	}
 }
@@ -59,24 +61,45 @@ for (let y = 0; y < 5; y++) {
 }
 
 
+let currentCards = [];
+let highlights = [];
+let selectedCell;
+function removeHighlights() {
+	if (selectedCell)
+		return;
+	for (let el of highlights)
+		el.remove();
+	highlights = [];
+}
+let latestData;
 function setBoard(data) {
+	removeHighlights();
+	latestData = data;
+	if (latestData.gameState != "in progress")
+		return console.error("game ended..");
 	const participating = localStorage["match-" + data.matchId];
 	const flipped = participating && participating[0] == "B";
 	const token = participating && participating.substr(1);
+	if (token)
+		board.el.setAttribute("playable", participating[0] == "B" ? "blue" : "red");
+	else
+		board.el.removeAttribute("playable");
+	board.el.setAttribute("turn", data.currentTurn);
 	if (flipped) {
 		board.el.setAttribute("flipped", "");
 		board.cards.red = board.cards.top;
 		board.cards.blue = board.cards.bottom;
 	} else {
+		board.el.removeAttribute("flipped");
 		board.cards.red = board.cards.bottom;
 		board.cards.blue = board.cards.top;
-		board.el.removeAttribute("flipped");
 	}
 	board.cards.blue[0].set(data.cards.blue[0]);
 	board.cards.blue[1].set(data.cards.blue[1]);
 	board.cards.side.set(data.cards.side);
 	board.cards.red[0].set(data.cards.red[0]);
 	board.cards.red[1].set(data.cards.red[1]);
+	currentCards = data.currentTurn == "blue" ? board.cards.blue : board.cards.red;
 
 	let newPieces = [[], [], [], []], i = 0, removedPieces = [[], [], [], []];
 	for (let y = 0; y < 5; y++)
@@ -94,34 +117,97 @@ function setBoard(data) {
 		let i = 0;
 		for (; i < Math.min(newPieces[type].length, removedPieces[type].length); i++) {
 			// move piece
-			const fromCell = newPieces[type][i];
-			const toCell = removedPieces[type][i];
+			const toCell = newPieces[type][i];
+			const fromCell = removedPieces[type][i];
 			toCell.piece = fromCell.piece;
-		}
-		for (; i < newPieces[type].length; i++) {
-			// create piece
-			const cell = newPieces[type][i];
-			cell.piece = {
-				el: document.createElement("game-piece"),
-				value: type + 1,
-			};
-			if (type < 2)
-				cell.piece.el.setAttribute("blue", "");
-			else
-				cell.piece.el.setAttribute("red", "");
-			if (type % 2 == 1)
-				cell.piece.el.setAttribute("master", "");
-			cell.piece.el.style.setProperty("--x", cell.x);
-			cell.piece.el.style.setProperty("--y", cell.y);
-			board.el.append(cell.piece.el);
-			window.requestAnimationFrame(_ => window.requestAnimationFrame(_ => cell.piece.el.style.setProperty("opacity", 1)));
+			fromCell.piece = undefined;
+			toCell.piece.x = toCell.x;
+			toCell.piece.y = toCell.y;
+			toCell.piece.el.style.setProperty("--x", toCell.x);
+			toCell.piece.el.style.setProperty("--y", toCell.y);
 		}
 		for (; i < removedPieces[type].length; i++) {
 			// delete piece
 			const cell = removedPieces[type][i];
+			const piece = cell.piece;
 			cell.piece.el.style.setProperty("opacity", 0);
-			window.requestAnimationFrame(_ => setTimeout(_ => cell.piece.el.remove(), 500));
+			cell.piece.el.style.setProperty("z-index", -1);
+			window.requestAnimationFrame(_ => setTimeout(_ => piece.el.remove(), 500));
 			cell.piece = undefined;
+		}
+		for (; i < newPieces[type].length; i++) {
+			// create piece
+			const cell = newPieces[type][i];
+			const piece = {
+				el: document.createElement("game-piece"),
+				value: type + 1,
+				x: cell.x,
+				y: cell.y,
+			};
+			cell.piece = piece;
+			let side = type < 2 ? "blue" : "red";
+			piece.el.setAttribute(side, "");
+			if (type % 2 == 1)
+				piece.el.setAttribute("master", "");
+			piece.el.style.setProperty("--x", cell.x);
+			piece.el.style.setProperty("--y", cell.y);
+			board.el.append(piece.el);
+			window.requestAnimationFrame(_ => window.requestAnimationFrame(_ => piece.el.style.setProperty("opacity", 1)));
+			piece.el.onclick = e => {
+				if (selectedCell)
+					return;
+				piece.el.onmouseenter();
+				selectedCell = cell;
+			}
+			piece.el.onmouseenter = _ => {
+				removeHighlights();
+				for (let i = 0; i < 25; i++) {
+					const y = piece.y - Math.floor(i / 5) + 2;
+					const x = piece.x + (i % 5) - 2;
+					if (x < 0 || x >= 5 || y < 0 || y >= 5)
+						continue;
+					if (board.cells[y][x].piece && ((board.cells[y][x].piece.value <= 2) == (piece.value <= 2)))
+						continue;
+					let useCard = [];
+					for (let j = 0; j < currentCards.length; j++)
+						if (currentCards[j].moves[piece.value <= 2 ? i : 24 - i] == "1") {
+							useCard.push(j);
+							continue;
+						}
+					if (!useCard.length)
+						continue;
+					let highlightEl = document.createElement("game-ghost-piece");
+					if (type % 2 == 1)
+						highlightEl.setAttribute("master", "");
+					highlightEl.style.setProperty("--x", x);
+					highlightEl.style.setProperty("--y", y);
+					board.el.append(highlightEl);
+					highlights.push(highlightEl);
+					highlightEl.onclick = _ => {
+						selectedCell = undefined;
+						const card = currentCards[useCard[0]].name;
+						const pos = "abcde"[piece.x] + (piece.y+1) + "abcde"[x] + (y+1);
+						ws.send("move " + latestData.matchId + " " + localStorage["match-" + latestData.matchId].substr(1) + " " + card + " " + pos);
+						let predictedBoard = latestData.board.split("");
+						predictedBoard[x + 5 * y] = predictedBoard[piece.x + 5 * piece.y];
+						predictedBoard[piece.x + 5 * piece.y] = '0';
+						let predictedCards = latestData.cards;
+						predictedCards.side = latestData.cards[latestData.currentTurn][useCard[0]];
+						predictedCards[latestData.currentTurn][useCard[0]] = latestData.cards.side;
+						setBoard({
+							gameState: latestData.gameState,
+							matchId: latestData.matchId,
+							currentTurn: latestData.currentTurn == "red" ? "blue" : "red",
+							startingCards: latestData.startingCards,
+							moves: latestData.moves.concat([card + ":" + pos]),
+							winner: "none",
+							board: predictedBoard.join(""),
+							cards: predictedCards,
+						})
+					};
+				}
+			};
+			piece.el.onmouseleave = removeHighlights;
 		}
 	}
 }
@@ -132,7 +218,6 @@ const ws = new WebSocket("wss://litama.herokuapp.com");
 ws.onerror = initialiseMainPage;
 ws.onmessage = e => {
 	const data = JSON.parse(e.data);
-	console.log(data);
 	switch(data.messageType) {
 	case "state":
 		let joining = false;
@@ -151,13 +236,17 @@ ws.onmessage = e => {
 		break;
 	case "spectate":
 		subscribed = true;
+	case "move":
 		break;
 	case "join":
 	case "create":
+		console.log(data);
 		localStorage["match-" + data.matchId] = (data.color == "red" ? "R" : "B") + data.token;
 		break;
 	case "error":
 	default:
+		if (data.command == "heartbeat")
+			return;
 		console.error(data);
 		if (data.command == "state") {
 			delete localStorage["match-" + match[1]];
@@ -170,8 +259,10 @@ ws.onmessage = e => {
 const match = document.location.hash.match(/^#([0-9a-f]+)$/i);
 if (match) {
 	ws.onopen = _ => {
+		setInterval(_ => ws.send("heartbeat"), 5000);
 		ws.send("state " + match[1]);
 	};
+	ws.onclose = _ => setTimeout(_ => window.location.reload(), 1000);
 } else
 	initialiseMainPage();
 
